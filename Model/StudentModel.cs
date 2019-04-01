21be0cfd6ba42d2c651abb1c844ef7d926a6c20f
NguyenTienDat <34731364+NguyenTienDat150699@users.noreply.github.com>
2019-04-01 12:52:25 +0700

    Create StudentModel

---
+++ Model/StudentModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class StudentModel
+    {
+        private int identity;
+        private string fullName;
+        private DateTime birthDate;
+
+        public StudentModel() { }
+        public int Identity
+        {
+            get { return identity; }
+            set { identity = value; }
+        }
+        public string FullName
+        {
+            get { return fullName; }
+            set { fullName = value; }
+        }
+        public string BirthDate
+        {
+            get { return birthDate.ToShortDateString(); }
+            set { DateTime.TryParse(value, out birthDate); }
+        }
+    }
+}
