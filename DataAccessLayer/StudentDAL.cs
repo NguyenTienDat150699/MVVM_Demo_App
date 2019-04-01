fe39916874f53b2e94a42932c50d1b8de5fb526e
NguyenTienDat <34731364+NguyenTienDat150699@users.noreply.github.com>
2019-04-01 13:16:04 +0700

    Create StudentDAL

---
+++ DataAccessLayer/StudentDAL.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infrastructure;
+using Model;
+
+namespace DataAccessLayer
+{
+    public class StudentDAL : IDatabase<StudentModel>
+    {
+        private OleDbConnection connection;
+        public StudentDAL()
+        {
+            connection = new OleDbConnection("");
+        }
+        public bool CreateItem(StudentModel item)
+        {
+            try
+            {
+                if (connection.State != ConnectionState.Open)
+                    connection.Open();
+                OleDbCommand command = new OleDbCommand(
+                    "INSERT INTO Student (FullName, BirthDate) VALUES (@fullname, @birthdate)",
+                    connection);
+                command.Parameters.Add("@fullname", OleDbType.BSTR).Value = item.FullName;
+                command.Parameters.Add("@birthdate", OleDbType.Date).Value = item.BirthDate;
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return true;
+        }
+
+        public bool DeleteItem(StudentModel item)
+        {
+            try
+            {
+                if (connection.State != ConnectionState.Open)
+                    connection.Open();
+                OleDbCommand command = new OleDbCommand(
+                    "DELETE FROM Student WHERE Identity = @identity", connection);
+                command.Parameters.Add("@identity", OleDbType.Numeric).Value = item.Identity;
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return true;
+        }
+
+        public DataTable ReadItems()
+        {
+            DataTable dataTable = new DataTable();
+            try
+            {
+                if (connection.State != ConnectionState.Open)
+                    connection.Open();
+                OleDbCommand command = new OleDbCommand(
+                    "SELETE * FROM Student ORDER BY Identity ASC", connection);
+                OleDbDataAdapter oleDbDataAdapter = new OleDbDataAdapter(command);
+                oleDbDataAdapter.Fill(dataTable);
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return dataTable;
+        }
+
+        public bool UpdateItem(StudentModel item)
+        {
+            try
+            {
+                if (connection.State != ConnectionState.Open)
+                    connection.Open();
+                OleDbCommand command = new OleDbCommand(
+                    "UPDATE Student SET FullName = @fullname, BirthDate = @birthdate " +
+                    "WHERE Identity = @identity", connection);
+                command.ExecuteNonQuery();
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return true;
+        }
+    }
+}
