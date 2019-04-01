b798e28f3bf9aa65991f1b7f1f1d6c1a00bd6254
NguyenTienDat <34731364+NguyenTienDat150699@users.noreply.github.com>
2019-04-01 12:46:31 +0700

    Create IDatabase interface

---
+++ Infrastructure/IDatabase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public interface IDatabase<T>
+    {
+        bool CreateItem(T item);
+        DataTable ReadItems();
+        bool UpdateItem(T item);
+        bool DeleteItem(T item);
+    }
+}
